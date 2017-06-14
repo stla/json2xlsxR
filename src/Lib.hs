832bbfe8deec933cb2ae8751c2aacd75e1363653
stla <laurent_step@yahoo.fr>
2017-06-14 21:00:13 +0200

    update

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Lib
   where
-import WriteXLSX
+import WriteXLSX (writeXlsx3)
 import Foreign
 import Foreign.C
 
-foreign export ccall json2xlsx :: Ptr CString -> Ptr CString -> IO ()
-json2xlsx :: Ptr CString -> Ptr CString -> IO ()
-json2xlsx json outfile = do
-  _json <- peek json
+foreign export ccall json2xlsx :: Ptr CString -> Ptr CString -> Ptr CString -> IO ()
+json2xlsx :: Ptr CString -> Ptr CString -> Ptr CString -> IO ()
+json2xlsx json1 json2 outfile = do
+  _json1 <- peek json1
+  _json2 <- peek json2
   _outfile <- peek outfile
-  json <- peekCString _json
+  json1 <- peekCString _json1
+  json2 <- peekCString _json2
   outfile <- peekCString _outfile
-  writeXlsx json outfile
+  writeXlsx3 json1 json2 outfile
