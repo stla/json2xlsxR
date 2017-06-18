54e23e6d2ac1e158a28383d393183fa846438451
stla <laurent_step@yahoo.fr>
2017-06-18 19:51:38 +0200

    peekCAString

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 module Lib
   where
-import JSONtoXLSX (writeXlsx4)
+import JSONtoXLSX (writeXlsx5)
 import Foreign
 import Foreign.C
-import Data.ByteString (packCString)
+-- import Data.ByteString (packCString)
+
+-----------------------------------------------
 -- import Data.Text
 -- import Data.Text.Encoding
 -- import Foreign.C.String
@@ -17,18 +19,28 @@ import Data.ByteString (packCString)
 --
 -- encode :: Text -> (CString -> IO a) -> IO a
 -- encode text = useAsCString (encodeUtf8 text)
+---------------------------------------------------
+
+-- -- OK avec iconv dans R:
+-- foreign export ccall json2xlsx :: Ptr CString -> Ptr CString -> Ptr CString -> IO ()
+-- json2xlsx :: Ptr CString -> Ptr CString -> Ptr CString -> IO ()
+-- json2xlsx json1 json2 outfile = do
+--   _json1 <- peek json1
+--   _json2 <- peek json2
+--   _outfile <- peek outfile
+--   -- json <- decode _json1
+--   -- json1 <- encode json peekCString
+--   -- json1 <- peekCString _json1
+--   -- json2 <- peekCString _json2
+--   json1 <- packCString _json1
+--   json2 <- packCString _json2
+--   outfile <- peekCString _outfile
+--   writeXlsx4 json1 json2 outfile
 
 foreign export ccall json2xlsx :: Ptr CString -> Ptr CString -> Ptr CString -> IO ()
 json2xlsx :: Ptr CString -> Ptr CString -> Ptr CString -> IO ()
 json2xlsx json1 json2 outfile = do
-  _json1 <- peek json1
-  _json2 <- peek json2
-  _outfile <- peek outfile
-  -- json <- decode _json1
-  -- json1 <- encode json peekCString
-  -- json1 <- peekCString _json1
-  -- json2 <- peekCString _json2
-  json1 <- packCString _json1
-  json2 <- packCString _json2
-  outfile <- peekCString _outfile
-  writeXlsx4 json1 json2 outfile
+  json1 <- (>>=) (peek json1) peekCAString 
+  json2 <- (>>=) (peek json2) peekCAString
+  outfile <- (>>=) (peek outfile) peekCAString
+  writeXlsx5 json1 json2 outfile
